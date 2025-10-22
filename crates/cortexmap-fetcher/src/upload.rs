1d136e75b5b18c0b7594ed00a2cb1c8360ce43c1
Sandipsinh Dilipsinh Rathod <62684960+ssddOnTop@users.noreply.github.com>
2025-10-21 20:22:15 -0400

    avoid failing on first err

--- crates/cortexmap-fetcher/src/upload.rs
+++ crates/cortexmap-fetcher/src/upload.rs
@@ -10,7 +10,7 @@ pub async fn upload<I: DatabaseInfra + S3Infra + Send + Sync + 'static>(
 ) -> Result<(), FetchError> {
     for stream in streams {
         // TODO: skip if the paper alr exists in the DB.
-        
+
         let key = determine_key(&stream.pmc_id, blueprint);
         // Map the stream to skip errors and unwrap Ok values
         let byte_stream = stream
@@ -21,15 +21,18 @@ pub async fn upload<I: DatabaseInfra + S3Infra + Send + Sync + 'static>(
             .put_s3(&key, ContentType::Pdf, Box::pin(byte_stream))
             .await;
         if let Ok(()) = res {
-            let paper = ctx.infra
+            // TODO: Upgrade err handling here.
+            ctx.infra
                 .insert_paper(NewPaper {
                     pmc_id: stream.pmc_id,
                     s3_key: key,
                     uid: uuid::Uuid::new_v4().to_string(),
                     query: blueprint.fetcher.query.clone(),
                 })
-                .await?;
-            tracing::info!("Uploaded paper: {:?}", paper);
+                .await
+                .map(|paper| {
+                    tracing::info!("Uploaded paper: {:?}", paper);
+                }).ok();
         }
     }
 
